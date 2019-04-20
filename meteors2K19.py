8a57a5d9d53da7ee21882e48579632cb1f63cde8
bl17zar <bl17zar@gmail.com>
2019-04-20 21:52:15 +0300

    added bullets

--- meteors2K19.py
+++ meteors2K19.py
@@ -6,11 +6,22 @@ pygame.init()
 FPS = 24
 SIZE = width, height = 640, 480
 SCREEN = pygame.display.set_mode(SIZE)
+BULLETS = []
 CLOCK = pygame.time.Clock()
 
 
 class GameObject:
-    def __init__(self, path_to_img, width=32, height=45, speed_x=0, speed_y=0, angle=0):
+    def __init__(
+        self,
+        path_to_img,
+        width=32,
+        height=45,
+        speed_x=0,
+        speed_y=0,
+        angle=0,
+        x=0,
+        y=0
+    ):
 
         # we keep the original surface to compensate rotation
         # lost of image quality
@@ -29,10 +40,20 @@ class GameObject:
         self.projection_x = 1
         self.projection_y = 0
 
-
-class Rocket(GameObject):
     def _perform_move(self):
         self.rect = self.rect.move([self.speed_x, self.speed_y])
+
+    def update(self):
+        print('angle: {}'.format(self.angle))
+        print('projection_x: {}'.format(self.projection_x))
+        print('projection_y: {}'.format(self.projection_y))
+        print('speed_x: {}'.format(self.speed_x))
+        print('speed_y: {}'.format(self.speed_y))
+        print('\n')
+        self._perform_move()
+
+
+class Rocket(GameObject):
 
     def _accelerate(self, val):
         self.speed_x += val * self.projection_x
@@ -81,19 +102,27 @@ class Rocket(GameObject):
         if direction == 'up':
             self._accelerate(1)
 
-        # y is inverted so we add minus before sin
         self.projection_x = -math.sin(self.rads)
         self.projection_y = -math.cos(self.rads)
 
-    def update(self):
-        print('angle: {}'.format(self.angle))
-        print('rads: {}'.format(self.rads))
-        print('projection_x: {}'.format(self.projection_x))
-        print('projection_y: {}'.format(self.projection_y))
-        print('speed_x: {}'.format(self.speed_x))
-        print('speed_y: {}'.format(self.speed_y))
-        print('\n')
-        self._perform_move()
+    def fire(self):
+        bullet_speed = 3
+
+        bullet = Bullet(
+            'assets/bullet.png',
+            width=30,
+            height=20,
+            speed_x=self.projection_x * bullet_speed + self.speed_x,
+            speed_y=self.projection_y * bullet_speed + self.speed_y,
+            angle=self.angle
+        )
+
+        BULLETS.append(bullet)
+
+
+class Bullet(GameObject):
+    def exist(self):
+        pass
 
 
 rocket = Rocket("assets/rocket.png", angle=-90)
@@ -111,12 +140,19 @@ while True:
     if keys[pygame.K_UP]:
         rocket.accept_direction('up')
 
+    if keys[pygame.K_SPACE]:
+        rocket.fire()
+
     rocket.update()
 
     CLOCK.tick(FPS)
 
     SCREEN.fill((0, 0, 0))
     SCREEN.blit(rocket.surface, rocket.rect)
+
+    for bullet in BULLETS:
+        SCREEN.blit(bullet.surface, bullet.rect)
+        bullet.update()
 
     pygame.display.flip()
     pygame.event.pump()
